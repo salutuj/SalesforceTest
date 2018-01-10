f127f5d2654d25c30203165c2d19148d1775fecf
Pawel Niewiadomski <pniewiadomski@si-ecommerce.pl>
2018-01-10 18:13:41 +0100

    more libs

--- src/ETClientTest.java
+++ src/ETClientTest.java
@@ -1,13 +1,28 @@
 import com.exacttarget.fuelsdk.*;
 
 public class ETClientTest {
+	static ETClient etClient;
 	
 	public static void main(String [] args){
 		try{		   
-			ETClient etClient = new ETClient();
-		    System.out.println(etClient.getAccessToken());		
+			etClient = new ETClient();
+			
+			testAccessToken();
+			testTriggeredEmail();
+		    		
 		} catch (Exception ex){
 			ex.printStackTrace();
 		}
+	}
+
+	private static void testAccessToken() {
+		System.out.println(etClient.getAccessToken());		
+	}
+
+	private static void testTriggeredEmail() throws ETSdkException {
+		ETTriggeredEmail email = new ETTriggeredEmail();
+		email.setClient(etClient);
+	    email.setKey("Finish_365_trigger_test");
+		email.send("pablodiablo@mailinator.com");
 	}
 }
